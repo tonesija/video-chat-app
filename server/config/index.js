884f64ed15b3378056dae4175d20f51027721b3f
Tonio Ercegović <te51681@fer.hr>
2021-03-22 23:29:32 +0100

    client build3

--- server/config/index.js
+++ server/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
         options: {
           dialect: 'postgres',
           protocol: 'postgres',
-          ssl = true,
-          dialectOptions = {
+          ssl: true,
+          dialectOptions: {
             ssl: true,
           },
           url: process.env.DATABASE_URL || "postgres://postgres:111111222@127.0.0.1:5432/VideoChatAppDB"
